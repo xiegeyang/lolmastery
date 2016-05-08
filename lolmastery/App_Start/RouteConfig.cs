9f76500306d71266ff2180dcc2d08cb1b3812486
Bluu <bluu@me.com>
2016-05-07 17:28:11 -0700

    Created project

--- lolmastery/App_Start/RouteConfig.cs
+++ lolmastery/App_Start/RouteConfig.cs
@@ -11,12 +11,10 @@ namespace lolmastery
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                name: "LOL",
+                url: "{*url}",
+                defaults: new { controller = "LOL", action = "Index" }
             );
         }
     }
---
+++ lolmastery/Controllers/LOLController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lolmastery.Controllers
+{
+    public class LOLController : Controller
+    {
+        // GET: LOL
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
